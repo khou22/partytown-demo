151981d87423397d07eedabbfef25cfe74a015c3
Kevin Hou <kevin.ch.hou@gmail.com>
2022-06-27 13:08:20 -0700

    Working partytown React example

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Partytown } from "@builder.io/partytown/react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Partytown Demo</h1>
+      <Partytown debug forward={["dataLayer.push"]} />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: 'console.log("Hello, Partytown");',
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
